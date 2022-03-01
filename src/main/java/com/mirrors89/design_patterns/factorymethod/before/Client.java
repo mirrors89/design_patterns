d5081244d81413cb48f0b875e255bb7235f2a2a2
이기승 <keeseung@sixshop.com>
2022-03-01 15:08:01 +0900

    factory method - init

---
+++ src/main/java/com/mirrors89/design_patterns/factorymethod/before/Client.java
@@ -0,0 +1,13 @@
+package com.mirrors89.design_patterns.factorymethod.before;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Ship whiteship = ShipFactory.orderShip("Whiteship", "keesun@mail.com");
+        System.out.println(whiteship);
+
+        Ship blackship = ShipFactory.orderShip("Blackship", "keesun@mail.com");
+        System.out.println(blackship);
+    }
+
+}
